d988aa120346cc28abd872892bc35214cb642a72
Francisco de Menezes Cavalcante Sassi <109996531+sassichico@users.noreply.github.com>
2025-06-04 17:14:27 -0300

    Add files via upload

---
+++ BioGeoBEARS Harttia/Harttia_BioGeoBEARS.R
@@ -0,0 +1,424 @@
+# Load the package 
+library(ape)
+library(optimx)   
+library(GenSA)    
+library(rexpokit)
+library(cladoRcpp)
+library(snow)     
+library(parallel)
+library(BioGeoBEARS)
+
+setwd("")
+
+getwd()
+
+extdata_dir = np(system.file("extdata", package="BioGeoBEARS"))
+extdata_dir
+list.files(extdata_dir)
+
+
+trfn = ("harttiatimecal.nwk")
+
+# Look at the raw Newick file:
+moref(trfn)
+
+# Look at your phylogeny (plots to a PDF, which avoids issues with multiple graphics in same window):
+pdffn = "tree.pdf"
+pdf(file=pdffn, width=9, height=9)
+
+tr = read.tree(trfn)
+tr
+plot(tr, cex=0.5)
+title("Harttia_Tree")
+
+
+dev.off()
+cmdstr = paste0("open ", pdffn)
+system(cmdstr)
+
+
+geogfn = ("geoharttia.data")
+
+# Look at the raw geography text file:
+moref(geogfn)
+
+# Look at your geographic range data:
+tipranges = getranges_from_LagrangePHYLIP(lgdata_fn=geogfn)
+tipranges
+
+# Maximum range size observed:
+max(rowSums(dfnums_to_numeric(tipranges@df)))
+
+# Set the maximum number of areas any species may occupy; this cannot be larger 
+# than the number of areas you set up, but it can be smaller.
+max_range_size = 2
+
+
+#######################################################
+#######################################################
+# DEC ANALYSIS
+#######################################################
+#######################################################
+# Run DEC
+#######################################################
+
+# Intitialize a default model (DEC model)
+BioGeoBEARS_run_object = define_BioGeoBEARS_run()
+
+# Give BioGeoBEARS the location of the phylogeny Newick file
+BioGeoBEARS_run_object$trfn = trfn
+
+# Give BioGeoBEARS the location of the geography text file
+BioGeoBEARS_run_object$geogfn = geogfn
+
+# Input the maximum range size
+BioGeoBEARS_run_object$max_range_size = max_range_size
+
+BioGeoBEARS_run_object$min_branchlength = 0.000001    
+BioGeoBEARS_run_object$include_null_range = TRUE    
+
+
+# Set up a time-stratified analysis:
+BioGeoBEARS_run_object$timesfn = "timeperiods.txt"
+BioGeoBEARS_run_object$dispersal_multipliers_fn = "dispersal_multipliers.txt"
+
+timeperiods = scan("timeperiods.txt")
+print(timeperiods)
+
+
+# Speed options and multicore processing
+BioGeoBEARS_run_object$on_NaN_error = -1e50    
+BioGeoBEARS_run_object$speedup = TRUE          
+BioGeoBEARS_run_object$use_optimx = TRUE    
+BioGeoBEARS_run_object$num_cores_to_use = 20
+
+# Sparse matrix exponentiation is an option for huge numbers of ranges/states (600+)
+BioGeoBEARS_run_object$force_sparse = FALSE   
+
+# Load the dispersal multiplier matrix from the text files into the model object
+BioGeoBEARS_run_object = readfiles_BioGeoBEARS_run(BioGeoBEARS_run_object)
+
+# Divide the tree up by timeperiods/strata
+BioGeoBEARS_run_object = section_the_tree(inputs=BioGeoBEARS_run_object, make_master_table=TRUE, plot_pieces=FALSE, fossils_older_than=10000, cut_fossils=FALSE)
+
+# The stratified tree is described in this table:
+BioGeoBEARS_run_object$master_table
+
+# Good default settings to get ancestral states
+BioGeoBEARS_run_object$return_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_TTL_loglike_from_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_ancprobs = TRUE    # get ancestral states from optim run
+
+# Set up DEC model
+
+# Look at the BioGeoBEARS_run_object
+BioGeoBEARS_run_object
+
+# Model object
+BioGeoBEARS_run_object$BioGeoBEARS_model_object
+
+# Parameters of the model 
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table
+
+# Check inputs
+BioGeoBEARS_run_object = fix_BioGeoBEARS_params_minmax(BioGeoBEARS_run_object=BioGeoBEARS_run_object)
+check_BioGeoBEARS_run(BioGeoBEARS_run_object)
+
+# load the saved result
+runslow = TRUE
+resfn = "Harttia_DEC_M0_unconstrained_v1.Rdata"
+if (runslow)
+{
+  res = bears_optim_run(BioGeoBEARS_run_object)
+  res    
+  
+  save(res, file=resfn)
+  resDEC = res
+} else {
+  # Loads to "res"
+  load(resfn)
+  resDEC = res
+}
+
+
+#######################################################
+# PDF plots
+#######################################################
+pdffn = "Harttia_DEC_M0_unconstrained_v1.pdf"
+pdf(pdffn, height=6, width=6)
+
+#######################################################
+# Plot ancestral states - DEC
+#######################################################
+analysis_titletxt ="BioGeoBEARS DEC on Harttia M0_unconstrained"
+
+# Setup
+results_object = resDEC
+scriptdir = np(system.file("extdata/a_scripts", package="BioGeoBEARS"))
+
+# States
+res2 = plot_BioGeoBEARS_results(results_object, analysis_titletxt, addl_params=list("j"), plotwhat="text", label.offset=0.45, tipcex=0.7, statecex=0.7, splitcex=0.6, titlecex=0.8, plotsplits=TRUE, cornercoords_loc=scriptdir, include_null_range=TRUE, tr=tr, tipranges=tipranges)
+
+# Pie chart
+plot_BioGeoBEARS_results(results_object, analysis_titletxt, addl_params=list("j"), plotwhat="pie", label.offset=0.45, tipcex=0.7, statecex=0.7, splitcex=0.6, titlecex=0.8, plotsplits=TRUE, cornercoords_loc=scriptdir, include_null_range=TRUE, tr=tr, tipranges=tipranges)
+
+
+dev.off()  # Turn off PDF
+cmdstr = paste("open ", pdffn, sep="")
+system(cmdstr) # Plot it
+
+#######################################################
+#######################################################
+# DIVALIKE
+# Run DIVALIKE
+#######################################################
+BioGeoBEARS_run_object = define_BioGeoBEARS_run()
+BioGeoBEARS_run_object$trfn = trfn
+BioGeoBEARS_run_object$geogfn = geogfn
+BioGeoBEARS_run_object$max_range_size = max_range_size
+BioGeoBEARS_run_object$min_branchlength = 0.000001   
+BioGeoBEARS_run_object$include_null_range = TRUE    
+
+# Set up a time-stratified analysis:
+BioGeoBEARS_run_object$timesfn = "timeperiods.txt"
+BioGeoBEARS_run_object$dispersal_multipliers_fn = "dispersal_multipliers.txt"
+
+# Speed options and multicore processing
+BioGeoBEARS_run_object$on_NaN_error = -1e50    
+BioGeoBEARS_run_object$speedup = TRUE          
+BioGeoBEARS_run_object$use_optimx = TRUE    
+BioGeoBEARS_run_object$num_cores_to_use = 20
+BioGeoBEARS_run_object$force_sparse = FALSE    
+
+# Loads the dispersal multiplier matrix from the text files into the model object
+BioGeoBEARS_run_object = readfiles_BioGeoBEARS_run(BioGeoBEARS_run_object)
+
+# Divide the tree up by timeperiods/strata 
+BioGeoBEARS_run_object = section_the_tree(inputs=BioGeoBEARS_run_object, make_master_table=TRUE, plot_pieces=FALSE, fossils_older_than=0.001, cut_fossils=FALSE)
+
+# The stratified tree is described in this table:
+BioGeoBEARS_run_object$master_table
+
+# Default settings to get ancestral states
+BioGeoBEARS_run_object$return_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_TTL_loglike_from_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_ancprobs = TRUE    # get ancestral states from optim run
+
+# Set up DIVALIKE model
+# Remove subset-sympatry
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["s","type"] = "fixed"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["s","init"] = 0.0
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["s","est"] = 0.0
+
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["ysv","type"] = "2-j"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["ys","type"] = "ysv*1/2"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["y","type"] = "ysv*1/2"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["v","type"] = "ysv*1/2"
+
+# Allow classic, widespread vicariance; all events equiprobable
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["mx01v","type"] = "fixed"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["mx01v","init"] = 0.5
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["mx01v","est"] = 0.5
+
+# No jump dispersal/founder-event speciation
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["j","type"] = "free"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["j","init"] = 0.01
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["j","est"] = 0.01
+
+BioGeoBEARS_run_object = fix_BioGeoBEARS_params_minmax(BioGeoBEARS_run_object=BioGeoBEARS_run_object)
+check_BioGeoBEARS_run(BioGeoBEARS_run_object)
+
+runslow = TRUE
+resfn = "Harttia_DIVALIKE_M0_unconstrained_v1.Rdata"
+if (runslow)
+{
+  res = bears_optim_run(BioGeoBEARS_run_object)
+  res    
+  
+  save(res, file=resfn)
+  resDIVALIKE = res
+} else {
+  # Loads to "res"
+  load(resfn)
+  resDIVALIKE = res
+}
+
+
+
+pdffn = "Harttia_DIVALIKE_M0_unconstrained_v1.pdf"
+pdf(pdffn, height=6, width=6)
+
+#######################################################
+# Plot ancestral states - DIVALIKE
+#######################################################
+analysis_titletxt ="BioGeoBEARS DIVALIKE on Harttia M0_unconstrained"
+
+# Setup
+results_object = resDIVALIKE
+scriptdir = np(system.file("extdata/a_scripts", package="BioGeoBEARS"))
+
+# States
+res2 = plot_BioGeoBEARS_results(results_object, analysis_titletxt, addl_params=list("j"), plotwhat="text", label.offset=0.45, tipcex=0.7, statecex=0.7, splitcex=0.6, titlecex=0.8, plotsplits=TRUE, cornercoords_loc=scriptdir, include_null_range=TRUE, tr=tr, tipranges=tipranges)
+
+# Pie chart
+plot_BioGeoBEARS_results(results_object, analysis_titletxt, addl_params=list("j"), plotwhat="pie", label.offset=0.45, tipcex=0.7, statecex=0.7, splitcex=0.6, titlecex=0.8, plotsplits=TRUE, cornercoords_loc=scriptdir, include_null_range=TRUE, tr=tr, tipranges=tipranges)
+
+
+dev.off()
+cmdstr = paste("open ", pdffn, sep="")
+system(cmdstr)
+
+#######################################################
+#######################################################
+# BAYAREALIKE
+#######################################################
+# Run BAYAREALIKE
+#######################################################
+BioGeoBEARS_run_object = define_BioGeoBEARS_run()
+BioGeoBEARS_run_object$trfn = trfn
+BioGeoBEARS_run_object$geogfn = geogfn
+BioGeoBEARS_run_object$max_range_size = max_range_size
+BioGeoBEARS_run_object$min_branchlength = 0.000001   
+BioGeoBEARS_run_object$include_null_range = TRUE
+
+# Set up a time-stratified analysis:
+BioGeoBEARS_run_object$timesfn = "timeperiods.txt"
+BioGeoBEARS_run_object$dispersal_multipliers_fn = "dispersal_multipliers.txt"
+
+
+# Speed options and multicore processing if desired
+BioGeoBEARS_run_object$on_NaN_error = -1e50    
+BioGeoBEARS_run_object$speedup = TRUE          
+BioGeoBEARS_run_object$use_optimx = TRUE    
+BioGeoBEARS_run_object$num_cores_to_use = 20
+BioGeoBEARS_run_object$force_sparse = FALSE    
+
+# Loads the dispersal multiplier matrix etc. from the text files into the model object
+BioGeoBEARS_run_object = readfiles_BioGeoBEARS_run(BioGeoBEARS_run_object)
+
+# Divide the tree up by timeperiods/strata 
+BioGeoBEARS_run_object = section_the_tree(inputs=BioGeoBEARS_run_object, make_master_table=TRUE, plot_pieces=FALSE, fossils_older_than=0.001, cut_fossils=FALSE)
+
+# The stratified tree is described in this table:
+BioGeoBEARS_run_object$master_table
+
+# Default settings to get ancestral states
+BioGeoBEARS_run_object$return_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_TTL_loglike_from_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_ancprobs = TRUE    # get ancestral states from optim run
+
+# Set up BAYAREALIKE model
+# No subset sympatry
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["s","type"] = "fixed"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["s","init"] = 0.0
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["s","est"] = 0.0
+
+# No vicariance
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["v","type"] = "fixed"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["v","init"] = 0.0
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["v","est"] = 0.0
+
+# No jump dispersal/founder-event speciation
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["j","type"] = "free"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["j","init"] = 0.01
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["j","est"] = 0.01
+
+# Adjust linkage between parameters
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["ysv","type"] = "1-j"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["ys","type"] = "ysv*1/1"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["y","type"] = "1-j"
+
+# Only sympatric/range-copying (y) events allowed, and with exact copying
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["mx01y","type"] = "fixed"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["mx01y","init"] = 0.9999
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["mx01y","est"] = 0.9999
+
+# Check the inputs; fixing any initial ("init") values outside min/max
+BioGeoBEARS_run_object = fix_BioGeoBEARS_params_minmax(BioGeoBEARS_run_object=BioGeoBEARS_run_object)
+check_BioGeoBEARS_run(BioGeoBEARS_run_object)
+
+runslow = TRUE
+resfn = "Harttia_BAYAREALIKE_M0_unconstrained_v1.Rdata"
+if (runslow)
+{
+  res = bears_optim_run(BioGeoBEARS_run_object)
+  res    
+  
+  save(res, file=resfn)
+  resBAYAREALIKE = res
+} else {
+  # Loads to "res"
+  load(resfn)
+  resBAYAREALIKE = res
+}
+
+
+pdffn = "Harttia_BAYAREALIKE_M0_unconstrained_v1.pdf"
+pdf(pdffn, height=6, width=6)
+
+#######################################################
+# Plot ancestral states - BAYAREALIKE
+#######################################################
+analysis_titletxt ="BioGeoBEARS BAYAREALIKE on Harttia M0_unconstrained"
+
+# Setup
+results_object = resBAYAREALIKE
+scriptdir = np(system.file("extdata/a_scripts", package="BioGeoBEARS"))
+
+# States
+res2 = plot_BioGeoBEARS_results(results_object, analysis_titletxt, addl_params=list("j"), plotwhat="text", label.offset=0.45, tipcex=0.7, statecex=0.7, splitcex=0.6, titlecex=0.8, plotsplits=TRUE, cornercoords_loc=scriptdir, include_null_range=TRUE, tr=tr, tipranges=tipranges)
+
+# Pie chart
+plot_BioGeoBEARS_results(results_object, analysis_titletxt, addl_params=list("j"), plotwhat="pie", label.offset=0.45, tipcex=0.7, statecex=0.7, splitcex=0.6, titlecex=0.8, plotsplits=TRUE, cornercoords_loc=scriptdir, include_null_range=TRUE, tr=tr, tipranges=tipranges)
+
+
+dev.off()
+cmdstr = paste("open ", pdffn, sep="")
+system(cmdstr)
+
+
+#######################################################
+# Statistics -- DEC
+#######################################################
+LnL_2 = get_LnL_from_BioGeoBEARS_results_object(resDEC)
+LnL_2
+
+# DEC, null model for Likelihood Ratio Test (LRT)
+res2 = extract_params_from_BioGeoBEARS_results_object(results_object=resDEC, returnwhat="table", addl_params=c("j"), paramsstr_digits=4)
+
+res2
+
+
+getAIC(-39.29528, 2)
+
+
+obj <- rbind(res2)
+tmp_tests = conditional_format_table(stats)
+
+restable = rbind(restable, res2, res1)
+teststable = rbind(teststable, tmp_tests)
+
+#######################################################
+# Statistics -- DIVALIKE
+#######################################################
+LnL_2 = get_LnL_from_BioGeoBEARS_results_object(resDIVALIKE)
+
+
+# DIVALIKE, null model for Likelihood Ratio Test (LRT)
+res2 = extract_params_from_BioGeoBEARS_results_object(results_object=resDIVALIKE, returnwhat="table", addl_params=c("j"), paramsstr_digits=4)
+
+getAIC(-38.80716, 2)
+
+#######################################################
+# Statistics -- BAYAREALIKE 
+#######################################################
+# We have to extract the log-likelihood differently, depending on the 
+# version of optim/optimx
+LnL_2 = get_LnL_from_BioGeoBEARS_results_object(resBAYAREALIKE)
+
+numparams2 = 2
+
+# BAYAREALIKE, null model for Likelihood Ratio Test (LRT)
+res2 = extract_params_from_BioGeoBEARS_results_object(results_object=resBAYAREALIKE, returnwhat="table", addl_params=c("j"), paramsstr_digits=4)
+
+getAIC(-45.32449, 2)
